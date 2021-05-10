9d03b9877374c0f90f334a92f2ccf1c0641d6615
Luis Fajardo <LuisFajardo1805@gmail.com>
2021-05-10 13:58:04 -0400

    Agg codigo js de la clase 1

---
+++ FundamentosDeJS/clase1.js
@@ -0,0 +1,11 @@
+var nombre = 'Luis' , apellido = 'Fajardo' 
+
+var nombreEnMayuscula = nombre.toUpperCase()
+
+var apellidoEnMiniscula = apellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+
+var cantidadDeLetrasDelNombre = nombre.length
+
+var nombreCompleto = `${nombre} ${apellido}`
